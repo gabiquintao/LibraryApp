5f071fe7b9e390fb8dfafd15ed806c76d265585b
Gabriel Araújo <a27978@alunos.ipca.pt>
2025-11-29 17:46:57 +0000

    Add User class with documentation, overriden ToString() method for User class

---
+++ LibraryApp/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.Entities
+{
+    public class User
+    {
+        private readonly int _id;
+        private string _name;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private static int _nextId = 0;
+
+		/// <summary>
+		/// Represents a user in the library.
+		/// </summary>
+		/// <param name="title">The title of the book.</param>
+		/// <param name="author">The author of the book.</param>
+		/// <param name="year">Publication year.</param>
+		public User(string name)
+        {
+            _id = _nextId++;
+            Name = name;
+        }
+    }
+}
