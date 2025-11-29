e7c36a59aedd07896c33c6fe7dd23badbf62f3b9
Gabriel Araújo <a27978@alunos.ipca.pt>
2025-11-29 22:11:28 +0000

    Update User class, added ToString() overriden method, added data validations

--- LibraryApp/Entities/User.cs
+++ LibraryApp/Entities/User.cs
@@ -4,20 +4,32 @@ using System.Text;
 
 namespace LibraryApp.Entities
 {
-    public class User
+	/// <summary>
+	/// Represents a user in the library.
+	/// </summary>
+	public class User
     {
         private readonly int _id;
-        private string _name;
+        private string _name = "";
 
         public int Id
         {
             get { return _id; }
         }
 
-        public string Name
+		/// <summary>
+		/// Name of the user.
+		/// </summary>
+		/// <exception cref="ArgumentException">Thrown when title is empty or whitespace.</exception>
+		public string Name
         {
             get { return _name; }
-            set { _name = value; }
+            set { 
+                if (string.IsNullOrWhiteSpace(value)) {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+                _name = value;
+            }
         }
 
         private static int _nextId = 0;
@@ -25,13 +37,20 @@ namespace LibraryApp.Entities
 		/// <summary>
 		/// Represents a user in the library.
 		/// </summary>
-		/// <param name="title">The title of the book.</param>
-		/// <param name="author">The author of the book.</param>
-		/// <param name="year">Publication year.</param>
+		/// <param name="name">The name of the user.</param>
+		/// <exception cref="ArgumentException">Thrown when any parameter is invalid.</exception>
 		public User(string name)
         {
             _id = _nextId++;
             Name = name;
         }
-    }
+
+		/// <summary>
+		/// Returns a string representation of the user.
+		/// </summary>
+		public override string ToString()
+		{
+			return $"{Id}: \"{Name}\"";
+		}
+	}
 }
