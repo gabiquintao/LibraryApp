d7fdf0aff3dabe6d218c909c15c0fc9d6f7179cc
Gabriel Araújo <a27978@alunos.ipca.pt>
2025-11-30 23:19:55 +0000

    Add IBookRepository contract with GetAll, FindById, and Add methods

---
+++ LibraryApp/Repositories/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using LibraryApp.Entities;
+using LibraryApp.Shared;
+
+namespace LibraryApp.Repositories
+{
+	/// <summary>
+	/// Defines the contract for a repository that manages Book entities.
+	/// </summary>
+	public interface IBookRepository
+	{
+		public Result<IReadOnlyCollection<Book>> GetAll();
+		public Result<Book> FindById(int id);
+		public Result Add(Book book);
+	}
+}
